48b687a8a625f071eb5beda1f1347fadf4f46e99
woonohyo <woonohyo@nhnnext.org>
2014-07-10 14:57:31 +0900

    Initial Commit

---
+++ iOSBasic/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iOSBasic
+//
+//  Created by Wonhyo Yi on 2014. 7. 10..
+//  Copyright (c) 2014년 Woonohyo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
