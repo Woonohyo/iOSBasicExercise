2e2ca1268df5750c59b4165c204ca01530110b43
woonohyo <woonohyo@nhnnext.org>
2014-07-17 15:09:42 +0900

    Week2 Added

---
+++ iOSBasic/Week2/Week2.h
@@ -0,0 +1,13 @@
+//
+//  Week2.h
+//  iOSBasic
+//
+//  Created by Wonhyo Yi on 2014. 7. 17..
+//  Copyright (c) 2014년 Woonohyo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Week2 : NSObject
+
+@end
---
+++ iOSBasic/Week2/Week2.m
@@ -0,0 +1,13 @@
+//
+//  Week2.m
+//  iOSBasic
+//
+//  Created by Wonhyo Yi on 2014. 7. 17..
+//  Copyright (c) 2014년 Woonohyo. All rights reserved.
+//
+
+#import "Week2.h"
+
+@implementation Week2
+
+@end
