37785fd647d40f03cf4d76fc68efe35d2b153f27
woonohyo <woonohyo@nhnnext.org>
2014-07-24 14:58:56 +0900

    Week3 init commit

---
+++ iOSBasic/Week3/Week3.m
@@ -0,0 +1,15 @@
+//
+//  Week3.m
+//  iOSBasic
+//
+//  Created by Wonhyo Yi on 2014. 7. 24..
+//  Copyright (c) 2014년 Woonohyo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    NSLog(@"Welcome to Week3");
+    return 0;
+}
