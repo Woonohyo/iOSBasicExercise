7bb5341063ae4d200998d5fceba311c72b13e090
woonohyo <woonohyo@nhnnext.org>
2014-07-24 16:59:53 +0900

    week2 이름 변경

---
+++ iOSBasic/Week2/Week2.m
@@ -0,0 +1,31 @@
+////
+////  Week2.m
+////  iOSBasic
+////
+////  Created by Wonhyo Yi on 2014. 7. 17..
+////  Copyright (c) 2014년 Woonohyo. All rights reserved.
+////
+//
+//#import <Foundation/Foundation.h>
+//#import "JsonParser.h"
+//
+//int main(int argc, const char * argv[])
+//{
+//    NSString * sampleJson = @"{“id”:007, “name”:“james”,“weapons”:“gun”}";
+//    NSString * sampleJson2 = @"{ “id” : 007, “name” : “james”, “weapons” : [ gun, pen ] }";
+//    NSString * sampleJson3 = @"[ { “id”: “001”, “name” : “john” }, { “id”: “007”, “name” : “james” } ]";
+//    
+//    JsonParser *jsonParser = [[JsonParser alloc] init];
+//    
+//    [jsonParser MyJSONSerializationFrom:sampleJson];
+//    /*
+//    
+//    //[jsonParser MyJSONSerializationFrom:sampleJson2];
+//    //[jsonParser MyJSONSerializationFrom:sampleJson3];
+//    
+//    NSLog(@"%@", [jsonParser MyJSONMakerWithDictionary:@{@"id":@"007", @"name":@"james",@"weapons":@"gun"}]);
+//    NSLog(@"%@", [jsonParser MyJSONMakerWithDictionary:@{@"id":@"007", @"name":@"james",@"weapons":@[@"gun", @"pen"]}]);
+//    NSLog(@"%@", [jsonParser MyJSONMakerWithArray:@[@"gun", @"pen"]]);
+//    */
+//    return 0;
+//}
