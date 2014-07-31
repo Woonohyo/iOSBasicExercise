80d5a35eb4a454eb00d607ad673e17f223511168
woonohyo <woonohyo@nhnnext.org>
2014-07-31 15:17:07 +0900

    Initial Commit

---
+++ iOSBasicNotification/iOSBasicNotification/WNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WNAppDelegate.h
+//  iOSBasicNotification
+//
+//  Created by Wonhyo Yi on 2014. 7. 31..
+//  Copyright (c) 2014년 Woonohyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSBasicNotification/iOSBasicNotificationTests/iOSBasicNotificationTests.m
@@ -0,0 +1,34 @@
+//
+//  iOSBasicNotificationTests.m
+//  iOSBasicNotificationTests
+//
+//  Created by Wonhyo Yi on 2014. 7. 31..
+//  Copyright (c) 2014년 Woonohyo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSBasicNotificationTests : XCTestCase
+
+@end
+
+@implementation iOSBasicNotificationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
